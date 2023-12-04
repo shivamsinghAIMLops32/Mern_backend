d14e5d6c7067446bde621ce8224bcb60bc13f2c8
shivamsinghAIMLops32 <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2023-12-04 14:46:21 +0530

    first commit

---
+++ JAVASCIRPT_basic/error.js
@@ -0,0 +1,8 @@
+console.log("hey")
+
+console.log("SHIVAM SINGH");
+
+//  let j k = 40;      wrong
+
+ var $k = 30;
+ console.log($k);
---
+++ JAVASCIRPT_basic/variables.js
@@ -0,0 +1,10 @@
+let k = 20;               /*most used*/
+console.log(k);
+
+var p = 30;               /*most used*/
+console.log(p);
+
+const l = "hey";          /*less used*/
+console.log(l);
+
+console.log("hey everyone");
