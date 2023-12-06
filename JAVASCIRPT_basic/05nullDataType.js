cda544ac552f8e333afb613645ea80b3dbb57f8d
shivamsinghAIMLops32 <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2023-12-06 18:10:17 +0530

    null file pushed

--- JAVASCIRPT_basic/05nullDataType.js
+++ JAVASCIRPT_basic/05nullDataType.js
@@ -1,3 +1,23 @@
 // non-zero value = It does stores something in it.
  const nonZero = 15
- console.log(nonZero);
+ console.log(typeof nonZero);
+
+ // zero value = 0
+ var Zero = 0;
+ console.log(typeof Zero);
+
+ // UNDEFINED IS blank space which can occupy something lateron , its type or kind of object it will store is yet to be define.
+ const Undefined = undefined;
+ let notDeclared;
+ console.log(typeof Undefined);
+ console.log(typeof notDeclared);
+
+
+ // null is a void space having null or empty value. empty is considered as an value.
+ const Null = null;
+ console.log('null is type of '+typeof Null);
+
+ let sum = 0;
+ sum = null;
+ console.log(sum);
+ console.table({nonZero, Zero, undefined, notDeclared, Null, sum});
