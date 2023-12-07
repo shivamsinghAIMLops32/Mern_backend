de910e19e8c690ebcb8499a0240d2ceba7a17091
shivamsinghAIMLops32 <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2023-12-07 23:10:46 +0530

    nested if else

---
+++ 02conditionalStatement/02nestedIfElse.js
@@ -0,0 +1,10 @@
+let isUserPrime = true;
+let isDiscoverPrime = false;
+
+if (isUserPrime) {
+  if (isDiscoverPrime) {
+    console.log('enjoy discovery content');
+  } else {
+    console.log("please buy discovery membership too as you already buyed prime membership");
+  }
+}else console.log("please buy prime subscription");
