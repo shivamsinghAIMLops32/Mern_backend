cef2fd4f50722df8e48b5a2de80a507411ce1a5e
shivamsinghAIMLops32 <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2023-12-07 22:49:48 +0530

    conditional

---
+++ 02conditionalStatement/01ifANdElse.js
@@ -0,0 +1,35 @@
+// if and else 
+
+const a = 5;
+
+if(a == 5){
+  console.log(a);
+}else console.log("else wala statement hu bhai");
+
+// student marks print marks good or not
+const marks = 40;
+if(marks <= 40){
+  console.log("you are failed");
+}
+else console.log(("you are pass"));
+
+
+// que 2
+let primeMember = true;
+if (primeMember) {
+  console.log("enjoy the content");
+} else {
+    console.log("please buy a subscription");
+}
+
+
+// hotstar subscription
+let isPrimeMember = false;
+let isUserVip = true;
+if (isPrimeMember) {
+  console.log("enjoy the content");
+}
+ else if (isUserVip) {
+  console.log("you are vip member");
+}
+else console.log("please buy a subscription");
